f91c73729695ea699d8bea3302d7a32208a3aa97
battlejoose <battlejoose@gmail.com>
2024-03-27 16:27:07 -0700

    added link to dextools pair

--- src/components/DextoolsWidget.js
+++ src/components/DextoolsWidget.js
@@ -3,10 +3,24 @@ import React from 'react';
 const DextoolsWidget = () => {
     return (
         <div>
+            <a href="https://www.dextools.io/app/en/solana/pair-explorer/F7SjY53cr4vSJu3PboTFh3cQrkHqTJoxfrNf5wJPLApV?t=1711581712501"
+               target="_blank"
+               rel="noopener noreferrer"
+               style={{
+                   display: 'inline-block',
+                   backgroundColor: 'white',
+                   padding: '8px',
+                   borderRadius: '4px',
+                   textDecoration: 'none',
+                   color: 'black', // Ensure text color contrasts with white background
+                   marginBottom: '10px' // Optional: Adds space between the link and the iframe
+               }}>
+                View Full DEXTools Chart
+            </a>
             <iframe
                 id="dextools-widget"
                 title="DEXTools Trading Chart"
-                width="500"
+                width="550"
                 height="400"
                 src="https://www.dextools.io/widget-chart/en/solana/pe-light/F7SjY53cr4vSJu3PboTFh3cQrkHqTJoxfrNf5wJPLApV?theme=dark&chartType=1&chartResolution=30&drawingToolbars=false"
                 style={{ border: 0}}
